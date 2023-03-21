ac27d0abcc0651dd3860fae00bd074176806f1c4
priyanshuSI <74817876+priyanshuSI@users.noreply.github.com>
2023-03-21 09:26:02 +0530

    Create todo.php

---
+++ todo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h1>To-Do List</h1>
+  <input type="text" id="task" placeholder="Add a task">
+  <button id="add">Add</button>
+  <ul id="tasks"></ul>
+</body>
+<script src="script.js"></script>
+</html>
